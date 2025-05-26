cf9e0f60b8185da9c3190ed49f185dfcff4c5ba6
manishamawrii <meghamawri@gmail.com>
2025-05-26 14:08:19 +0530

    First commit

---
+++ ttt.js
@@ -0,0 +1,82 @@
+let boxes = document.querySelectorAll(".boxes")
+let showresult = document.querySelector(".showresult")
+let reset = document.querySelector(".restart")
+
+let currentPlayer = "❌"
+let game = ["", "", "" , "" , "", "" , "" , "" , "" ]
+let gameOn = true;
+
+
+let winnermatrix = [
+    [0,1,2] , [3,4,5] , [6,7,8] ,[0,3,6] , [1,4,7] ,[2,5,8] , [0,4,8], [2,4,6]
+]
+
+
+let handleclick = (e) =>
+    {
+       let index = e.target.dataset.index;
+       console.log(e.target)
+       console.log(index)
+
+       if(game[index]!=="" || !gameOn ){
+        return
+       }
+       
+       game[index] =currentPlayer;
+       console.log(" e",e.target)
+       e.target.textContent = currentPlayer;
+        currentPlayer=currentPlayer=="❌"?"⭕":"❌"
+       seewhoisthewinner();
+       
+    }
+
+    let seewhoisthewinner= ()=>{
+        for (const element of winnermatrix) {
+           let [a,b,c]= element
+           if(game[a]&&game[a]==game[b]&&game[b]==game[c] ) {
+
+            showresult.textContent= `🎉 🌟congratulation ${currentPlayer} is winner `
+            showresult.style.backgroundColor ="green"
+            gameOn=false;
+            return
+           }
+          
+        }
+
+        if(!game.includes(""))
+        {
+            gameOn= false;
+            showresult.textContent=`its a draw `
+            return;
+        }
+
+       
+
+
+    }
+
+
+let handlereset =()=>
+{
+     game.fill("");
+     gameOn = true;
+     currentPlayer="❌"
+     showresult.textContent= ""
+     boxes.forEach( box=> {
+        box.textContent=""
+        
+     });
+}
+
+
+
+
+boxes.forEach(boxes => {
+    boxes.addEventListener("click",handleclick)
+    
+});
+
+reset.addEventListener("click" , handlereset)
+// Destructuring is a feature in JavaScript that allows you to unpack values from arrays or objects into separate variables in a clean and readable way.
+
+
